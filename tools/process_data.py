0cade47d3df3f1aeb2d2bde836e748682f246768
JONGSKY <>
2023-12-15 04:05:15 +0900

    update code

--- tools/process_data.py
+++ tools/process_data.py
@@ -1,4 +1,11 @@
 from loguru import logger
+import sys
+import os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.abspath(os.path.join(current_directory, '..'))
+
+sys.path.append(parent_directory)
 
 from data_modori.config import init_configs
 from data_modori.core import Executor
